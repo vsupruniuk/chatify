146b6c0077a1dcddeef1bf3444c0a7f63ee0ecd1
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2026-03-27 11:25:50 +0200

    Create helloWorldTitlee.component.ts

---
+++ src/containers/helloWorldTitle/helloWorldTitlee.component.ts
@@ -0,0 +1,25 @@
+import {
+	ChangeDetectionStrategy,
+	Component,
+	inject,
+	OnInit,
+	signal,
+	WritableSignal,
+} from '@angular/core';
+
+import { HelloWorldService } from '@services';
+
+@Component({
+	selector: 'ctf-hello-world-title',
+	templateUrl: './helloWorldTitlee.component.html',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HelloWorldTitle implements OnInit {
+	private readonly _helloWorldService: HelloWorldService = inject(HelloWorldService);
+
+	protected readonly title: WritableSignal<string> = signal<string>('');
+
+	public ngOnInit(): void {
+		this.title.set(this._helloWorldService.getTitle());
+	}
+}
